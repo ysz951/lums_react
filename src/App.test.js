6db48f7dc957fef52cb22b9a7cdf4c6ce9afa660
ysz951 <shengyang_zhou@hotmail.com>
2021-03-02 00:15:52 -0600

    Change config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { BrowserRouter } from 'react-router-dom';
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>, div);
+  ReactDOM.unmountComponentAtNode(div)
+})
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    LUMS_API_URL: "http://localhost:5000/api",
+    LUMS_API_URL: "https://evening-thicket-36204.herokuapp.com/api",
     EMAIL_SERVICE_ID: process.env.REACT_APP_EMAIL_SERVICE_ID,
     EMAIL_USER_ID: process.env.REACT_APP_EMAIL_USER_ID,
     EMAIL_TEMPLATE_ID: process.env.REACT_APP_EMAIL_TEMPLATE_ID,
