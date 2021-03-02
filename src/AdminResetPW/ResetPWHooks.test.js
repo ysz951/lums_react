f534fa54fa66ec9560fbfb007aa1685040b4fa96
ysz951 <shengyang_zhou@hotmail.com>
2021-03-02 00:35:51 -0600

    Fix test bugs

--- src/AdminResetPW/ResetPWHooks.test.js
+++ src/AdminResetPW/ResetPWHooks.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import ResetPWHooks from './ResetPWHooks';
-
+import ResetPWHooks from './ResetPWHooks';;
+import { ContextProvider } from '../Context/Context';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <ResetPWHooks />
+      <ContextProvider>
+        <ResetPWHooks />
+      </ContextProvider>
     </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/Header/HeaderHooks.test.js
+++ src/Header/HeaderHooks.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import HeaderHooks from './HeaderHooks';
-
+import { ContextProvider } from '../Context/Context';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <HeaderHooks />
+      <ContextProvider>
+        <HeaderHooks />
+      </ContextProvider>
     </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div)
 })
