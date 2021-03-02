e93a4ff6ec97fb712f721508e3f07986a01832fe
ysz951 <shengyang_zhou@hotmail.com>
2021-03-02 16:36:09 -0600

    Change config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-    LUMS_API_URL: "https://evening-thicket-36204.herokuapp.com/api",
-    // LUMS_API_URL: "http://localhost:5000/api",
+    LUMS_API_URL: process.env.REACT_APP_API_URL,
     EMAIL_SERVICE_ID: process.env.REACT_APP_EMAIL_SERVICE_ID,
     EMAIL_USER_ID: process.env.REACT_APP_EMAIL_USER_ID,
     EMAIL_TEMPLATE_ID: process.env.REACT_APP_EMAIL_TEMPLATE_ID,
